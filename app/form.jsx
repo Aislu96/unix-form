f07765104a059ff5bb0566eb592b03c950e8218e
aislusha96 <arslanova1996ais@list.ru>
2024-09-27 19:09:45 +0300

    project

--- app/form.jsx
+++ app/form.jsx
@@ -3,8 +3,23 @@
 import PhoneInput from "react-phone-number-input";
 import { useState, useEffect } from "react";
 import "react-phone-number-input/style.css";
-import { validateName, validateEmail, validatePhone } from "./validators";
+import { validateName, validateEmail, validatePhone } from "./validators"
+import { initializeApp } from "firebase/app";
+import { getAuth, RecaptchaVerifier , signInWithPhoneNumber} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlsXrz-qcwU5ChHWoJ0o2L9wKBwP3ZZaE",
+  authDomain: "unix-form.firebaseapp.com",
+  projectId: "unix-form",
+  storageBucket: "unix-form.appspot.com",
+  messagingSenderId: "177613514383",
+  appId: "1:177613514383:web:578f0f76d70b21e49fb2fa",
+  measurementId: "G-TNQM2MT70Z"
+};
+
 const Form = ({ t, locale }) => {
+
+
   useEffect(() => {
     console.log(t, "t");
   }, [t]);
@@ -18,8 +33,46 @@ const Form = ({ t, locale }) => {
   //   const [checkboxChecked, setCheckboxChecked] = useState(false);
   //   const [checkboxError, setCheckboxError] = useState("");
   const [defaultCountry, setDefaultCountry] = useState("US");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isCodeSent, setIsCodeSent] = useState(false);
+  const [confirmationResult, setConfirmationResult] = useState(null);
+
+  const handlePhoneSubmit = async () => {
+    const appVerifier = window.recaptchaVerifier;
+    const phoneNumber = inputValuePhone;
+    try {
+      const confirmationResult = await signInWithPhoneNumber(getAuth(), phoneNumber, appVerifier);
+      setConfirmationResult(confirmationResult);
+      setIsCodeSent(true);
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+      alert("Ошибка отправки кода. Пожалуйста, попробуйте еще раз.");
+    }
+  };
+
+  const handleCodeSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await confirmationResult.confirm(verificationCode);
+      alert("Аутентификация прошла успешно!");
+    } catch (error) {
+      console.error("Error confirming code:", error);
+      alert("Неверный код. Пожалуйста, попробуйте снова.");
+    }
+  };
 
   useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    auth.languageCode = 'it';
+
+    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': () => {
+        handlePhoneSubmit();
+      }
+    }, auth);
+
     const fetchCountry = async () => {
       try {
         const response = await fetch("https://ipapi.co/json/");
@@ -33,7 +86,8 @@ const Form = ({ t, locale }) => {
     fetchCountry();
   }, []);
 
-  const handleChange = (eventOrValue, name) => {
+
+    const handleChange = (eventOrValue, name) => {
     if (name === "telephone") {
       setInputValuePhone(eventOrValue);
       setPhoneError(validatePhone(eventOrValue));
@@ -54,6 +108,7 @@ const Form = ({ t, locale }) => {
         break;
     }
   };
+
 
   const handleSubmit = async (event) => {
     // const validPhone = isValidPhoneNumber(inputValuePhone);
@@ -93,6 +148,7 @@ const Form = ({ t, locale }) => {
           locale: locale,
         };
 
+
         try {
           console.log(formData, "formData");
           const response = await fetch("/api/sendMessage", {
@@ -104,6 +160,7 @@ const Form = ({ t, locale }) => {
           });
           console.log(formData, "formData");
           if (response.ok) {
+            handlePhoneSubmit(event);
             // Handle success (e.g., display a success message)
             // Scroll to the form section
           } else {
@@ -154,11 +211,13 @@ const Form = ({ t, locale }) => {
     }
   };
 
+
   return (
     <div className="rounded-t-3xl h-full xl:absolute bottom-0 xl:max-w-[582px] max-h-[527px] xl:h-fit p-4 -mt-28 xl:mt-0 z-20   bg-[#F7F7FA]">
       <p className=" border-l-4 xl:border-none border-[#8861FF] text-xl px-4 my-4 font-medium ">
         {t["form head text"]}
       </p>
+      {!isCodeSent ? (
       <form action="" className="space-y-4">
         <input
           value={inputValueName}
@@ -217,14 +276,25 @@ const Form = ({ t, locale }) => {
           <p className=" text-red-500 border border-red-500 ">{phoneError}</p>
         )}
         <button
+            id="sign-in-button"
           onClick={handleSubmit}
           className="bg-[#8C56FF] text-xl font-me w-full rounded-full text-center text-white p-3"
         >
           {t["Get the Guide"]}
         </button>
-      </form>
+      </form>) : (
+              <form onSubmit={handleCodeSubmit}>
+                <input
+                    type="text"
+                    value={verificationCode}
+                    onChange={(e) => setVerificationCode(e.target.value)}
+                    placeholder="Введите код"
+                />
+                <button type="submit">approve code</button>
+              </form>
+      )}
     </div>
-  );
-};
+  )
+}
 
 export default Form;
